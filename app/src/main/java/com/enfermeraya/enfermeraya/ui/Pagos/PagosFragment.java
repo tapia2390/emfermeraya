0174bd1554832647238933b2ebcaff9765ca1e88
Yhonatan <tapia2390@gmail.com>
2020-07-09 09:34:49 -0500

    disain tarjeta

--- app/src/main/java/com/enfermeraya/enfermeraya/ui/Pagos/PagosFragment.java
+++ app/src/main/java/com/enfermeraya/enfermeraya/ui/Pagos/PagosFragment.java
@@ -23,13 +23,7 @@ public class PagosFragment extends Fragment {
         galleryViewModel =
                 ViewModelProviders.of(this).get(PagosViewModel.class);
         View root = inflater.inflate(R.layout.fragment_pagos, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
